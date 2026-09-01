333bd6a505d6ccc201ab1b1513622ca2ed3851be
PebPeb <bmkeen@liberty.edu>
2026-09-01 00:21:32 -0400

    Updated Tests

--- example-2/fifo_tb.py
+++ example-2/fifo_tb.py
@@ -4,8 +4,11 @@ from cocotb.clock import Clock
 from cocotb.triggers import Timer, RisingEdge
 import logging
 
-async def fifo_dump(dut, logger):
-  logger.log("Hello")
+testData = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 
+            0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F]
+
 
 async def fifo_write(dut, data):
   dut.data_in.value = data
@@ -14,6 +17,12 @@ async def fifo_write(dut, data):
   await RisingEdge(dut.clk)
   dut.wr.value = 0
 
+async def fifo_read(dut):
+  dut.rd.value = 1
+
+  await RisingEdge(dut.clk)
+  dut.rd.value = 0
+
 @cocotb.test()
 async def fifo_tb(dut):
   logger = logging.getLogger("my_testbench")
@@ -21,8 +30,7 @@ async def fifo_tb(dut):
   # Initialize System Clock
   clock = Clock(dut.clk, 10, units="ns")
   cocotb.start_soon(clock.start())
-  testData = [0xAA, 0xBB, 0xCC, 0xDD]
-
+  
   dut.reset.value = 1
   dut.data_in.value = 0x00
   dut.rd.value = 0
@@ -33,7 +41,29 @@ async def fifo_tb(dut):
 
   dut.reset.value = 0
 
+  assert dut.empty.value == 1, Exception()
+
   for data in testData:
     logger.info(data)
     await fifo_write(dut, data)
+
+  await RisingEdge(dut.clk)
+
+  assert dut.full.value == 1, Exception()
+  assert dut.empty.value == 0, Exception()
+  assert dut.wr_rdy.value == 0, Exception()
+
+  await Timer(40, unit="ns")
+
+  for i in range(32):
+    await fifo_read(dut)
+
+  await RisingEdge(dut.clk)
+
+  assert dut.full.value == 0, Exception()
+  assert dut.empty.value == 1, Exception()
+  assert dut.rd_rdy.value == 0, Exception()
+
+  await Timer(40, unit="ns")
+
   
